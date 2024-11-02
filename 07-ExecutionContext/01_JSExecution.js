783d87844bf00ac0c3de9e4d25e3245ccf4e3e76
Sameer <sameer07x19@gmail.com>
2024-11-03 00:45:09 +0530

    add files

---
+++ 07-ExecutionContext/01_JSExecution.js
@@ -0,0 +1,19 @@
+// In Js there are 2 Execution Context :
+
+// - Global Execution Context
+// - Function Execution Context
+
+// File execution phases :
+
+// - Memory execution phase 
+// - Execution phase 
+
+// 1. JS creates Global excution context 
+// 2. Next it creates memory phase
+// 3. Memory phase - In this phase,the variables are loaded and set to undefined 
+//    and the functions are set to their definitions.
+// 4. Next it creates the execution phase
+// 5. Execution phase - In this phase,the variables are initialsed to given values and 
+//    when the functions are called it creates the same memory phase and execution phase for the function 
+//    just like the main one.
+
